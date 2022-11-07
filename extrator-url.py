a3e863c63bc06e3c3e396ae7bc6b33107d01d561
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-11-07 20:57:07 -0300

    find

--- extrator-url.py
+++ extrator-url.py
@@ -1,9 +1,15 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
 print(url_base)
 
 
-url_param = url[20:]
+url_param = url[indice_interrogacao+1:]
 print(url_param)
+parametro_busca = 'moedaDestino'
+indice_parametro = url_param.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_param[indice_valor:]
+print(valor)
