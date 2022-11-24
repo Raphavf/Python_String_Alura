f2fa9a06a409d602961653dee4c4dcf3cfa9ce3a
Raphavf <47055553+Raphavf@users.noreply.github.com>
2022-11-23 21:36:04 -0300

    Classe ExtratorUrl

--- extrator-url.py
+++ extrator-url.py
@@ -1,15 +1,21 @@
-url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-print(url)
-
+#url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = ""
+if url == "":
+    raise ValueError("A url está vazia")
+#Separa base e os parâmetro
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
 print(url_base)
-
-
 url_param = url[indice_interrogacao+1:]
+
+#Busca o valor de um parâmetro
 print(url_param)
-parametro_busca = 'moedaDestino'
+parametro_busca = 'quantidade'
 indice_parametro = url_param.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_param[indice_valor:]
+indice_e_comercial = url_param.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_param[indice_valor:]
+else:
+    valor = url_param[indice_valor:indice_e_comercial]
 print(valor)
